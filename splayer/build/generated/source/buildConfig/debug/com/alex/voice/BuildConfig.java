2bf93bcb0b7f22d8c371dbfebe34ac4cd6951fde
chengchauanchen <1070669138@qq.com>
2022-06-15 10:53:05 +0800

    答题拆红包 版本

---
+++ splayer/build/generated/source/buildConfig/debug/com/alex/voice/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.alex.voice;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "com.alex.voice";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 35;
+  public static final String VERSION_NAME = "1.3.5";
+}
